512567671ff2362e5e7c3ee7981809f1fcd161ab
Malidudle <132945899+Malidudle@users.noreply.github.com>
2024-08-07 23:32:39 +0100

    Update clients.tsx

--- src/components/clients.tsx
+++ src/components/clients.tsx
@@ -1,7 +1,23 @@
-"use client";
+import Link from "next/link";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <div className="px-4 py-6 md:px-6 md:py-12 lg:py-16">
+      <h2 className="text-7xl font-bold">Clients</h2>
+      <ul className="mt-16 list-inside list-disc space-y-4">
+        <li className="hover:underline">
+          <Link target="_blank" href="https://tayvallichbay.com">
+            Tayvallich Bay Association
+          </Link>
+        </li>
+        <li className="hover:underline">
+          <Link target="_blank" href="https://disbyte.co.uk">
+            Disbyte
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Clients;
