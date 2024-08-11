90a5578798fbe86de16379388054dbdf8f648ac9
Malidudle <132945899+Malidudle@users.noreply.github.com>
2024-08-11 15:30:30 +0100

    Update loading-screen.tsx

--- src/components/loading-screen.tsx
+++ src/components/loading-screen.tsx
@@ -8,10 +8,19 @@ interface LoadingScreenProps {
 
 const LoadingScreen: React.FC<LoadingScreenProps> = ({ progress }) => {
   return (
-    <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black">
-      <div className="mb-4 text-2xl text-white">Loading...</div>
+    <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black text-white">
+      <div className="mb-20">
+        <h1 className="mb-4 text-center text-4xl font-bold">
+          Malin Grafton .com
+        </h1>
+        <p className="mb-4 max-w-sm text-center text-sm">
+          Welcome to my website! Please allow a few seconds for the page to
+          load.
+        </p>
+      </div>
+      <div className="mb-4 text-2xl">Loading...</div>
       <Progress value={progress} max={100} className="h-2 w-96" />
-      <div className="mt-2 text-white">{Math.round(progress)}%</div>
+      <div className="mt-2">{Math.round(progress)}%</div>
     </div>
   );
 };
